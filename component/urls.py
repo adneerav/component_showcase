57b1776f2be9f5df4f47469227670a71ef7e64a1
toneerav@gmail.com <toneerav@gmail.com>
2020-05-18 20:51:31 +0530

    Front ,Home, detail & listing

--- component/urls.py
+++ component/urls.py
@@ -4,5 +4,10 @@ from component import views
 
 # app_name = "component"
 urlpatterns = [
-    # path('', views.home),
+    path('', views.home, name='home'),
+    path('components/<str:name>/', views.components, name='component_tech'),
+    url('components/', views.component_list, name='component_list'),
+    path('component/detail/<int:component_id>/', views.detail_by_id, name='comp_detail_by_id'),
+    url('details', views.detail, name='component_detail'),
+
 ]
--- component/views.py
+++ component/views.py
@@ -4,3 +4,90 @@ from django.shortcuts import render, get_list_or_404
 # Create your views here.
 from component.models import Component, Detail
 from technology.models import Technology
+
+
+def home(request):
+    data = {
+        "technology_list": _get_technology(),
+        "components": _get_components(),
+    }
+    return render(request, 'components/home.html', context=data)
+
+
+def components(request, name):
+    print(name)
+    qs_component_all = {
+        "components": _get_technology_components(technology_name=name),
+        'is_technology_wise': True,
+        'technology_detail': _get_technology_detail(technology_name=name),
+        "title": "{} Components".format(name)
+    }
+    return render(request, 'components/components_page.html', context=qs_component_all)
+
+
+def component_list(request):
+    qs_component_all = {
+        "components": _get_components(),
+        'is_technology_wise': False,
+        "title": "All Components"
+    }
+    return render(request, 'components/components_page.html', context=qs_component_all)
+
+
+def detail_by_id(request, component_id):
+    if request.method == 'GET':
+        print(component_id)  # component_data
+        component_detail = Detail.objects.get(pk=component_id)
+        language_list = component_detail.language.all()
+        data = {
+            "detail": component_detail,
+            "languages": language_list,
+            "dev_detail": {
+                "name": "John",
+                "nickname": "Jimmy",
+                "team": "Yo Yo"
+            }
+        }
+        return render(request, 'components/detail.html', data)
+    else:
+        raise PermissionDenied
+
+
+def detail(request):
+    if request.method == 'POST':
+        component_id = request.POST["btn_component_detail"];
+        print(request.POST["btn_component_detail"])  # component_data
+        component_detail = Detail.objects.get(pk=component_id)
+        language_list = component_detail.language.all()
+        data = {
+            "detail": component_detail,
+            "languages": language_list,
+            "dev_detail": {
+                "name": "John",
+                "nickname": "Jimmy",
+                "team": "Yo Yo"
+            }
+        }
+        return render(request, 'components/detail.html', data)
+    else:
+        raise PermissionDenied
+
+
+def _get_technology_components(technology_name):
+    qs_tech_components = get_list_or_404(Detail, technology__name__iexact=technology_name)
+    return qs_tech_components
+
+
+def _get_technology_detail(technology_name):
+    technology_detail = Technology.objects.get(name=technology_name)
+    return technology_detail
+
+
+def _get_components():
+    qs_comp_list = Detail.objects.all()
+    return qs_comp_list
+
+
+def _get_technology():
+    qs_technologies = Technology.objects.all()
+    return qs_technologies
