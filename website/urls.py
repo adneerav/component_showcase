2c5ff28badb2dfaa24645e18f97d10eb64445f62
Mayur Kanojiya <kanojiyamayur@gmail.com>
2020-05-19 18:16:04 +0530

    add component added

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from website.views import home, add
+from website.views import home, add, add_detail
 
 urlpatterns = [
     path('', home, name='home'),
-    path('add', add, name= 'add')
+    path('add', add, name='add'),
+    path('addDetail', add_detail, name='add_detail')
 ]
--- website/views.py
+++ website/views.py
@@ -1,24 +1,41 @@
 from django.shortcuts import render, redirect
-from component.models import Detail
+from component.models import Detail, Component
 
 from django.forms import modelform_factory
 
-DetailForm = modelform_factory(Detail, exclude=[])
+ComponentInstance = modelform_factory(Component, exclude=[])
+DetailInstance = modelform_factory(Detail, exclude=[])
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'website/home.html')
 
+
 def add(request):
     if request.method == 'POST':
-        form = DetailForm(request.POST)
+        form = ComponentInstance(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_detail')
+    else:
+        form = ComponentInstance()
+
+    return render(request,
+                  'website/add.html',
+                  context={'form': form})
+
+
+def add_detail(request):
+    if request.method == 'POST':
+        form = DetailInstance(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
     else:
-        form = DetailForm()
+        form = DetailInstance()
 
     return render(request,
-                  'website/add.html',
+                  'website/add_detail.html',
                   context={'form': form})
