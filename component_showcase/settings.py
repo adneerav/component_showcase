51ed94b34262cad016b31cac5c85ccf8fa35de21
toneerav@gmail.com <toneerav@gmail.com>
2020-06-02 21:02:33 +0530

    changes regarding local server setup

--- component_showcase/settings.py
+++ component_showcase/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'drc^l83u=ae3^)s%ai7+!q3dcg9#cmbn$%za1xd7ob#&8+6g=)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
